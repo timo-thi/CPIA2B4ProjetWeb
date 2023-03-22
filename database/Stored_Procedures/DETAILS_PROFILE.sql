e2b7ca66ebf00689696fc92264d0b079e81040d2
Baptiste <baptistedelavenne80@gmail.com>
2023-03-22 10:46:23 +0100

    Create DETAILS_PROFILE.sql

---
+++ database/Stored_Procedures/DETAILS_PROFILE.sql
@@ -0,0 +1,11 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DETAILS_PROFILE`(p_id_profile INT)
+BEGIN
+SELECT profile.id_profile, profile.fname, profile.lname, profile.role, prom.id_prom, prom.name as promo, campus.id_campus, campus.name as campus, city.name as city
+FROM profile
+INNER JOIN affiliated ON profile.id_profile = affiliated.id_profile
+INNER JOIN prom ON affiliated.id_prom = prom.id_prom
+INNER JOIN campus ON prom.id_campus = campus.id_campus
+INNER JOIN address ON campus.id_address = address.id_address
+INNER JOIN city ON address.id_city = city.id_city
+WHERE profile.id_profile = p_id_profile;
+END
