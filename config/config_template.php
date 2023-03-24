296308f5612149a64676a16b580a6331a601ff85
Splash Mind <85194871+timo-thi@users.noreply.github.com>
2023-03-24 09:26:22 +0100

    config adjustement

---
+++ config/config_template.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	'db_name' => 'web', // Nom de la base de données dans le serv
+	'db_user' => '', // Nom d'utilisateur de la base de données
+	'db_pass' => '', // Mot de passe de la base de données
+	'db_host' => '', // Adresse du serveur de la base de données (ex : loaclhost:port)
+);
