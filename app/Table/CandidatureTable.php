f5da4a77011451ce93ce89d9f476febffe0f1870
Baptiste <baptistedelavenne80@gmail.com>
2023-03-25 21:55:48 +0100

    add controller

---
+++ app/Table/CandidatureTable.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Table;
+
+
+use \App\Entity\CandidatureEntity;
+use \Core\Table\Table;
+
+
+class CandidartureTable extends Table {
+
+	protected $table = 'candidature';
+²²²²
+}
