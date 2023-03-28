de02f57b4236320d7dc171f46c0ff74a78a460e6
Baptiste <baptistedelavenne80@gmail.com>
2023-03-28 22:48:21 +0200

    Update Complete_dump.sql

--- database/Complete_dump.sql
+++ database/Complete_dump.sql
@@ -926,8 +926,13 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `CREATION_WISH`(`p_id_profile` INT, `p_id_offer` INT)
 BEGIN
-insert into wish (id_profile,id_offer) values (p_id_profile,p_id_offer);
-END ;;
+    if exists(select id_profile, id_offer FROM wish WHERE id_profile = p_id_profile AND id_offer = p_id_offer )
+    then
+        select true; delete from wish where id_profile = p_id_profile AND id_offer = p_id_offer;
+    else
+        select false; insert into wish (id_profile,id_offer) values (p_id_profile,p_id_offer);
+    end if;
+END;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
