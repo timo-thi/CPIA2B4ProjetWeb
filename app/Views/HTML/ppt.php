300b2a81f86882d21bf8fcc964625911073b8dc7
DamianLari <119122227+DamianLari@users.noreply.github.com>
2023-03-29 12:45:39 +0200

    EDE

---
+++ app/Views/HTML/ppt.php
@@ -0,0 +1,29 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../BootStrap/assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../BootStrap/assets/css/style.css">
+    <link rel="stylesheet" href="../../../BootStrap/assets/vendors/fontawesome/css/all.min.css">
+    <title>Recherche d'entreprise</title>
+</head>
+
+
+
+
+<body class="bg-sombre ">
+    <div class="bg-clair mx-auto mt-5 rounded-5" style="width:80%; height:20%">
+    
+    </div>
+    d <br>
+    <div class="bg-sombre clair px-2 mt-5 mx-auto rounded-3 x-auto" style="width:max-content;"><p > Suivant</p></div>
+   
+
+<?php require './Footer.php'?>
+    <script src="../../../BootStrap/assets/vendors/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="../../../BootStrap/assets/vendors/jquery/jquery.min.js"></script>
+</body>
+
+</html>
